3a04d4ddb62e16e2e051b54258d955c523bee172
LuisOlivo340 <39189778+LuisOlivo340@users.noreply.github.com>
2019-10-06 21:21:26 -0500

    Avance

---
+++ ClienteProyecto/ClienteProyecto/Lobby.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClienteProyecto {
+    /// <summary>
+    /// Lógica de interacción para Lobby.xaml
+    /// </summary>
+    public partial class Lobby : Window {
+        List<String> vs = new List<string>();
+        public Lobby() {
+            InitializeComponent();
+            
+        }
+
+
+        private void chatLB_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void enviarTF_Click(object sender, RoutedEventArgs e) {
+            vs.Add(mensajeTF.Text);
+            chatLB.ItemsSource = new List<String>();
+            chatLB.ItemsSource = vs;
+        }
+    }
+}
---
+++ ClienteProyecto/ClienteProyecto/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClienteProyecto {
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void inicioBT_Click(object sender, RoutedEventArgs e) {
+            String usuario = usuarioTB.Text;
+            String contrasenia = ComputeSha256Hash(contraseniaTB.Password);
+            MenuPrincipal menuPrincipal = new MenuPrincipal();
+            menuPrincipal.Show();
+            this.Close();
+        }
+
+        private void validarBT_Click(object sender, RoutedEventArgs e) {
+            Validar validar = new Validar();
+            validar.Show();
+            this.Close();
+        }
+
+        private void registrarBT_Click(object sender, RoutedEventArgs e) {
+            Registro registro = new Registro();
+            registro.Show();
+            this.Close();
+        }
+
+        private string ComputeSha256Hash(string rawData) {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create()) {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++) {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
